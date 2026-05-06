7556a4ea27c913b29fe601be8a0a9281b6ed7dd5
Muhammad Ekhthiar Uddin <138322481+Niloy11111@users.noreply.github.com>
2026-05-06 18:57:40 +0600

    feat(routing): set up role-based routing structure and create folders for admin, doctor, and patient

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,19 @@
 import { Hero } from "@/components/modules/Hero";
 import { PatientReviews } from "@/components/modules/PatientReview";
 import { TopDoctors } from "@/components/modules/TopDoctors";
-
+import Head from "next/head";
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>AI-Powered Healthcare - Find Your Perfect Doctor</title>
+        <meta
+          name="description"
+          content="Discover top-rated doctors tailored to your needs with our AI-powered healthcare platform. Get personalized recommendations and book appointments effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Hero />
       <TopDoctors />
       <PatientReviews />
