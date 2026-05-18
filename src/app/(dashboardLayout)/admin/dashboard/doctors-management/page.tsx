e0746543f99b5bbbf6a2a70dbf67dd50c61e3933
Muhammad Ekhthiar Uddin <138322481+Niloy11111@users.noreply.github.com>
2026-05-18 12:35:29 +0600

    show avatar component

--- src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
+++ src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -1,5 +1,42 @@
-const AdminDoctorsManagementPage = () => {
-  return <div>AdminDoctorsManagementPage</div>;
+import DoctorsManagementHeader from "@/components/modules/Admin/DoctorManagement/DoctorsManagementHeader";
+import RefreshButton from "@/components/shared/RefreshButton";
+import SearchFilter from "@/components/shared/SearchFilter";
+import SelectFilter from "@/components/shared/SelectFilter";
+import { queryStringFormatter } from "@/lib/formatters";
+
+import { getDoctors } from "@/services/admin/doctorManagement";
+import { getSpecialities } from "@/services/admin/specialitiesManagement";
+import { ISpecialty } from "@/types/specialities.interface";
+
+const AdminDoctorsManagementPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+  const searchParamsObj = await searchParams;
+  const queryString = queryStringFormatter(searchParamsObj); // {searchTerm: "John", speciality: "Cardiology" => "?searchTerm=John&speciality=Cardiology"}
+  const specialitiesResult = await getSpecialities();
+  const doctorsResult = await getDoctors(queryString);
+  const totalPages = Math.ceil(
+    doctorsResult.meta.total / doctorsResult.meta.limit,
+  );
+  return (
+    <div className="space-y-6">
+      <DoctorsManagementHeader specialities={specialitiesResult.data} />
+      <div className="flex space-x-2">
+        <SearchFilter paramName="searchTerm" placeholder="Search doctors..." />
+        <SelectFilter
+          paramName="speciality" // ?speciality="Cardiology"
+          options={specialitiesResult.data.map((speciality: ISpecialty) => ({
+            label: speciality.title,
+            value: speciality.title,
+          }))}
+          placeholder="Filter by speciality"
+        />
+        <RefreshButton />
+      </div>
+    </div>
+  );
 };
 
 export default AdminDoctorsManagementPage;
