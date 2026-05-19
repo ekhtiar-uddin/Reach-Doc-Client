bbaf2eb5c7b0210c59bd63d16a2b2c1d1f3bb07c
Muhammad Ekhthiar Uddin <138322481+Niloy11111@users.noreply.github.com>
2026-05-19 14:40:42 +0600

    feat: create updateAdmin service function

--- src/services/admin/adminManagement.ts
+++ src/services/admin/adminManagement.ts
@@ -181,3 +181,21 @@ export async function updateAdmin(
     };
   }
 }
+
+/**
+ * SOFT DELETE ADMIN
+ * API: DELETE /admin/soft/:id
+ */
+export async function softDeleteAdmin(id: string) {
+  try {
+    const response = await serverFetch.delete(`/admin/soft/${id}`);
+    const result = await response.json();
+    return result;
+  } catch (error: any) {
+    console.log(error);
+    return {
+      success: false,
+      message: `${process.env.NODE_ENV === "development" ? error.message : "Something went wrong"}`,
+    };
+  }
+}
