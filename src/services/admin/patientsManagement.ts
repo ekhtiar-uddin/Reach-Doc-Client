51f3e31f85f945aea39cf815614635993f336ca9
Muhammad Ekhthiar Uddin <138322481+Niloy11111@users.noreply.github.com>
2026-05-19 15:17:01 +0600

    feat: add updatePatient service funtion

--- src/services/admin/patientsManagement.ts
+++ src/services/admin/patientsManagement.ts
@@ -40,3 +40,57 @@ export async function getPatientById(id: string) {
     };
   }
 }
+
+/**
+ * UPDATE PATIENT
+ * API: PATCH /patient/:id
+ */
+export async function updatePatient(
+  id: string,
+  _prevState: any,
+  formData: FormData,
+) {
+  const validationPayload: any = {
+    name: formData.get("name") as string,
+    contactNumber: formData.get("contactNumber") as string,
+    address: formData.get("address") as string,
+  };
+
+  const validation = zodValidator(validationPayload, updatePatientZodSchema);
+  if (!validation.success && validation.errors) {
+    return {
+      success: false,
+      message: "Validation failed",
+      formData: validationPayload,
+      errors: validation.errors,
+    };
+  }
+
+  if (!validation.data) {
+    return {
+      success: false,
+      message: "Validation failed",
+      formData: validationPayload,
+      errors: [{ field: "unknown", message: "Invalid data" }],
+    };
+  }
+  try {
+    const response = await serverFetch.patch(`/patient/${id}`, {
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(validation.data),
+    });
+
+    const result = await response.json();
+    return result;
+  } catch (error: any) {
+    console.error("Update patient error:", error);
+    return {
+      success: false,
+      message:
+        process.env.NODE_ENV === "development"
+          ? error.message
+          : "Failed to update patient",
+      formData: validationPayload,
+    };
+  }
+}
