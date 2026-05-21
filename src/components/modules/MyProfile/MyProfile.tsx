d85978444cefd9a4eadbb25ef7433bafc0dbd163
Muhammad Ekhthiar Uddin <138322481+Niloy11111@users.noreply.github.com>
2026-05-21 23:01:17 +0600

    feat: add resetPassword service function zod schemas

--- src/components/modules/MyProfile/MyProfile.tsx
+++ src/components/modules/MyProfile/MyProfile.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { getInitials } from "@/lib/formatters";
+import { updateMyProfile } from "@/services/auth/auth.service";
 
 import { UserInfo } from "@/types/user.interface";
 import { Camera, Loader2, Save } from "lucide-react";
