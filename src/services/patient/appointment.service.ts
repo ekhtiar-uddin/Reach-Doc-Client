e884a13a0c1c5f6fd28d7b42a07bd3f48476593f
Muhammad Ekhthiar Uddin <138322481+Niloy11111@users.noreply.github.com>
2026-05-31 18:24:32 +0600

    feat: added changeAppointmentStatus service function in patient

--- src/services/patient/appointment.service.ts
+++ src/services/patient/appointment.service.ts
@@ -89,3 +89,32 @@ export async function getAppointmentById(appointmentId: string) {
     };
   }
 }
+
+export async function changeAppointmentStatus(
+  appointmentId: string,
+  status: string,
+) {
+  try {
+    const response = await serverFetch.patch(
+      `/appointment/status/${appointmentId}`,
+      {
+        body: JSON.stringify({ status }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    const result = await response.json();
+    return result;
+  } catch (error: any) {
+    console.error("Error changing appointment status:", error);
+    return {
+      success: false,
+      message:
+        process.env.NODE_ENV === "development"
+          ? error.message
+          : "Failed to change appointment status",
+    };
+  }
+}
