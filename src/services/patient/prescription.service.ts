76d2af18e24b7ed4215d587a638076d93af0bcbf
Muhammad Ekhthiar Uddin <138322481+Niloy11111@users.noreply.github.com>
2026-05-31 18:25:19 +0600

    feat: added createPrescription service function in patient

---
+++ src/services/patient/prescription.service.ts
@@ -0,0 +1,27 @@
+"use server";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { serverFetch } from "@/lib/server-fetch";
+import { IPrescriptionFormData } from "@/types/prescription.interface";
+
+export async function createPrescription(data: IPrescriptionFormData) {
+  try {
+    const response = await serverFetch.post("/prescription", {
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+    return result;
+  } catch (error: any) {
+    console.error("Error creating prescription:", error);
+    return {
+      success: false,
+      message:
+        process.env.NODE_ENV === "development"
+          ? error.message
+          : "Failed to create prescription",
+    };
+  }
+}
